92d1cf0455ae309cecb5fbd20de67a0033d625c9
elay6 <elaynafl06@gmail.com>
2022-09-01 09:43:09 -0500

    Initial commit

---
+++ src/Bunny.java
@@ -0,0 +1,8 @@
+public class Bunny {
+    public static void main(String[] args) {
+        System.out.println("  (\\__//) ");
+        System.out.println("  ( ._. )");
+        System.out.println("  ( ) ( )");
+        System.out.println(" ('')_('')  ");
+    }
+}
